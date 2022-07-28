5c0ba577108709cefb685fa8719a32bb5939fa6e
Chip Hill <hillkarl03@gmail.com>
2022-07-28 13:32:08 -0400

    testing with sequelize sync options

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
